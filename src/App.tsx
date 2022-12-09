867ec9110104a74301e148ef256c55933eee8d3f
martindzida <martin.d14@email.cz>
2022-12-09 23:30:50 +0100

    Images, bad parallax, navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,33 @@
+import { Parallax, ParallaxLayer } from '@react-spring/parallax'
+import NavLink from './NavLink'
+import a from './assets/1.jpg'
+import b from './assets/2.jpg'
+import c from './assets/3.jpg'
+import d from './assets/4.jpg'
+import e from './assets/5.jpg'
+import f from './assets/6.jpg'
+import g from './assets/7.jpg'
+
 function App() {
-  return <div className='font-martian'></div>
+  return (
+    <div className='w-full h-full flex justify-center font-martian'>
+      <div className='w-full fixed flex justify-end gap-10 font-extralight uppercase text-white z-10 px-12 py-8'>
+        <NavLink href='/'>Home</NavLink>
+        <NavLink href='/'>About</NavLink>
+      </div>
+      <Parallax pages={2}>
+        <ParallaxLayer
+          speed={0.05}
+          factor={2}
+          style={{
+            backgroundSize: 'cover',
+            backgroundImage: `url(${a})`,
+          }}
+        ></ParallaxLayer>
+        <ParallaxLayer speed={0.5} factor={1.5} style={{ backgroundSize: 'cover', backgroundImage: `url(${g})` }}></ParallaxLayer>
+      </Parallax>
+    </div>
+  )
 }
 
 export default App
---
+++ src/NavLink.tsx
@@ -0,0 +1,17 @@
+interface Props {
+  href: string
+  children: string
+}
+
+const NavLink = ({ href, children }: Props) => {
+  return (
+    <a
+      href={href}
+      className='relative hover:after:w-full after:w-0 after:transition-width after:duration-500 after:ease-in-out after:absolute after:block after:h-0.5 after:bg-white'
+    >
+      {children}
+    </a>
+  )
+}
+
+export default NavLink
